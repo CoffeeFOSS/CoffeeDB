91aa9321955902dfc289063b9066efcd88a131f4
robchendev <robchendev@gmail.com>
2025-10-02 13:36:49 -0700

    [feat] add not found and server error routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -6,12 +6,16 @@ import { SignInComponent } from './components/signin/signin.component';
 import { RegisterComponent } from './components/register/register.component';
 import { signedInGuard } from './guards/signedIn.guard';
 import { TestErrorsComponent } from './components/test-errors/test-errors.component';
+import { ServerErrorComponent } from './components/server-error/server-error.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainframeComponent,
     children: [
+      { path: 'not-found', component: NotFoundComponent },
+      { path: 'server-error', component: ServerErrorComponent },
       { path: 'test-errors', component: TestErrorsComponent },
       { path: 'users', component: UserDirectoryComponent },
       {
