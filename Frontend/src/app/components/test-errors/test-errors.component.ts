fcc4ce02db40ea2514b6fb674ef8c08a0e23b1b8
robchendev <robchendev@gmail.com>
2025-10-02 13:37:11 -0700

    [feat] add buttons to invoke errors

--- Frontend/src/app/components/test-errors/test-errors.component.ts
+++ Frontend/src/app/components/test-errors/test-errors.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { HotToastService } from '@ngxpert/hot-toast';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-test-errors',
@@ -9,15 +11,76 @@ import { HotToastService } from '@ngxpert/hot-toast';
 })
 export class TestErrorsComponent {
   toast = inject(HotToastService);
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  validationErrors: string[] = [];
 
   // https://ngxpert.github.io/hot-toast/
   onClickToastError() {
     this.toast.error('This is an error message');
   }
+
   onClickToastSuccess() {
     this.toast.success('This is a success message');
   }
+
   onClickToastInfo() {
     this.toast.info('This is an info message');
+  }
+  
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+        this.validationErrors = error;
+      },
+    });
   }
 }
