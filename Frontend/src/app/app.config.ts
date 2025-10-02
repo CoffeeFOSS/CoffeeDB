f804a38d60ea810334ed76df9636dc7e50c3a60b
Robert Chen <robchendev@gmail.com>
2025-10-02 11:29:03 -0700

    [Feat] Notification Toasts (#9)
    * feat: install toast service
    
    * feat: create test errors route with toast trigger buttons

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,13 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor } from './interceptors/jwt.interceptor';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([jwtInterceptor])),
+    provideHotToastConfig(),
   ],
 };
