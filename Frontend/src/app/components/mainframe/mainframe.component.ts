2a693a06f91b7628058807c8e572138cc6b67715
robchendev <robchendev@gmail.com>
2025-10-04 19:52:39 -0700

    [feat] add sidebar to mainframe

--- Frontend/src/app/components/mainframe/mainframe.component.ts
+++ Frontend/src/app/components/mainframe/mainframe.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from "../sidebar/sidebar.component";
 
 @Component({
   selector: 'app-mainframe',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, HeaderComponent, SidebarComponent],
   templateUrl: './mainframe.component.html',
   styleUrl: './mainframe.component.scss',
 })
