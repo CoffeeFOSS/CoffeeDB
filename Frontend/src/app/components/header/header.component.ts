0d4e83a684da08e2a0a8f148f8f00aa8cf4954d5
robchendev <robchendev@gmail.com>
2025-10-04 21:33:48 -0700

    [refactor] move handle out of header as component

--- Frontend/src/app/components/header/header.component.ts
+++ Frontend/src/app/components/header/header.component.ts
@@ -1,40 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-// import { UserMenuComponent } from '../user-menu/user-menu.component';
-import { AccountService } from '../../services/account.service';
 import { SidebarService } from '../../services/sidebar.service';
+import { UserHandleComponent } from '../user-handle/user-handle.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  // imports: [FormsModule, UserMenuComponent], // TODO: Change to reactive forms later on
-  imports: [FormsModule], // TODO: Change to reactive forms later on
+  imports: [FormsModule, UserHandleComponent], // TODO: Change to reactive forms later on
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  accountService = inject(AccountService);
-  userMenuOpen = false;
   public sidebar = inject(SidebarService);
-  private router = inject(Router);
 
   onOpenSidebar() {
     this.sidebar.open();
-  }
-
-  onSignInClick() {
-    this.router.navigate(['/signin'], {
-      queryParams: { redirectUrl: this.router.url },
-    });
-  }
-
-  onSignOutClick() {
-    this.accountService.signOut();
-    this.router.navigateByUrl('/');
-  }
-
-  onUserMenuClick() {
-    this.userMenuOpen = !this.userMenuOpen;
   }
 }
