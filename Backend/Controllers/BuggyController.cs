8738c28ba85ec2ada82858cf82322ccfc9ed76dc
robchendev <robchendev@gmail.com>
2025-10-06 17:38:50 -0700

    [docs] produces response type attributes for BuggyController

--- Backend/Controllers/BuggyController.cs
+++ Backend/Controllers/BuggyController.cs
@@ -10,12 +10,15 @@ public class BuggyController(DataContext context) : BaseApiController
 {
   [Authorize]
   [HttpGet("auth")]
+  [ProducesResponseType(200)]
+  [ProducesResponseType(401)]
   public ActionResult<string> GetAuth()
   {
     return "secret text";
   }
 
   [HttpGet("not-found")]
+  [ProducesResponseType(404)]
   public ActionResult<User> GetNotFound()
   {
     var thing = context.Users.Find(-1); // produce not found user
@@ -24,6 +27,7 @@ public class BuggyController(DataContext context) : BaseApiController
   }
 
   [HttpGet("server-error")]
+  [ProducesResponseType(500)]
   public ActionResult<User> GetServerError() // 5xx = server error
   {
     var thing = context.Users.Find(-1) ?? throw new Exception("A bad thing has happened"); // generate null reference error
@@ -32,6 +36,7 @@ public class BuggyController(DataContext context) : BaseApiController
   }
 
   [HttpGet("bad-request")]
+  [ProducesResponseType(400)]
   public ActionResult<string> GetBadRequest() // 4xx = user error
   {
     return BadRequest("This was not a good request");
