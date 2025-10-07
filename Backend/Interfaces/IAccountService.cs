43291a9ea9291544a7bf2eaeaadf1fad64131a1d
robchendev <robchendev@gmail.com>
2025-10-06 22:44:26 -0700

    [docs] summary on defined methods for Account and User Service

--- Backend/Interfaces/IAccountService.cs
+++ Backend/Interfaces/IAccountService.cs
@@ -5,7 +5,17 @@ namespace Backend.Interfaces;
 
 public interface IAccountService
 {
-  // TODO: Summary
+  /// <summary>
+  /// Registers a new user.
+  /// </summary>
+  /// <param name="registerDto">The registration details.</param>
+  /// <returns><see cref="UserDto"/> Registered user information.</returns>
   Task<ServiceResult<UserDto>> RegisterAsync(RegisterDto registerDto);
+
+  /// <summary>
+  /// Logs in an existing user.
+  /// </summary>
+  /// <param name="loginDto">The login credentials.</param>
+  /// <returns><see cref="UserDto"/> Logged in user information.</returns>
   Task<ServiceResult<UserDto>> LoginAsync(LoginDto loginDto);
 }
--- Backend/Interfaces/IUserService.cs
+++ Backend/Interfaces/IUserService.cs
@@ -4,7 +4,16 @@ namespace Backend.Interfaces;
 
 public interface IUserService
 {
-  // TODO: Summary
+  /// <summary>
+  /// Gets all users.
+  /// </summary>
+  /// <returns>A list of user information.</returns>
   Task<IEnumerable<MemberDto>> GetUsersAsync();
+
+  /// <summary>
+  /// Gets a user by their username.
+  /// </summary>
+  /// <param name="username">The username of the user.</param>
+  /// <returns>The user information.</returns>
   Task<MemberDto> GetUserAsync(string username);
 }
