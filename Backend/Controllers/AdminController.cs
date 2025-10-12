1798db245fb5160b0f2232ec640303ba9be263dc
robchendev <robchendev@gmail.com>
2025-10-11 18:28:22 -0700

    [feat] edit-roles/username endpoint

--- Backend/Controllers/AdminController.cs
+++ Backend/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
+using Backend.Common;
 using Backend.Entities;
+using Backend.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers;
 
+// TODO: Refactor to AdminService
 public class AdminController(UserManager<User> userManager) : BaseApiController
 {
   [Authorize(Policy = "RequireAdminRole")]
@@ -22,5 +25,27 @@ public class AdminController(UserManager<User> userManager) : BaseApiController
       }).ToListAsync();
 
     return Ok(users);
+  }
+
+  [Authorize(Policy = "RequireAdminRole")]
+  [HttpPost("edit-roles/{username}")]
+  public async Task<IActionResult> EditRoles(string username, string roles)
+  {
+    if (string.IsNullOrEmpty(roles)) return ServiceResult<List<string>>.Failure(400, "Must select at least one role").ToActionResult();
+
+    var selectedRoles = roles.Split(",").ToArray();
+
+    var user = await userManager.FindByNameAsync(username);
+    if (user == null) return ServiceResult<List<string>>.Failure(400, "User not found").ToActionResult();
+
+    var userRoles = await userManager.GetRolesAsync(user);
+
+    var result = await userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+    if (!result.Succeeded) return ServiceResult<List<string>>.Failure(400, "Failed to add to roles").ToActionResult();
+
+    result = await userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+    if (!result.Succeeded) return ServiceResult<List<string>>.Failure(400, "Failed to remove from roles").ToActionResult();
+
+    return Ok(await userManager.GetRolesAsync(user));
   }
 }
