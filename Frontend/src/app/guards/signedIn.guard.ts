b22b4f7ebbac28cb278369c8337168ee79c66cd1
robchendev <robchendev@gmail.com>
2025-10-14 15:24:44 -0700

    [feat]: admin section on sidebar

--- Frontend/src/app/guards/signedIn.guard.ts
+++ Frontend/src/app/guards/signedIn.guard.ts
@@ -1,17 +1,16 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AccountService } from '../services/account.service';
-import { HotToastService } from '@ngxpert/hot-toast';
 
 export const signedInGuard: CanActivateFn = (route, state) => {
   // Guard to check if user is simply logged in / authenticated.
   const accountService = inject(AccountService);
   const router = inject(Router);
-  const toast = inject(HotToastService);
 
   if (!accountService.currentUser()) {
-    router.navigateByUrl('/');
-    toast.error('You cannot access this page');
+    router.navigate(['/signin'], {
+      queryParams: { redirectUrl: state.url },
+    });
     return false;
   }
   return true;
