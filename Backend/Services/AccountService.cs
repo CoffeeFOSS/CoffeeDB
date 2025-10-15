314448718cb0e2f45db2c49aeb74f3c4816e312a
robchendev <robchendev@gmail.com>
2025-10-14 21:56:06 -0700

    [cleanup] remove obsolete comments

--- Backend/Services/AccountService.cs
+++ Backend/Services/AccountService.cs
@@ -66,8 +66,6 @@ public class AccountService(UserManager<User> userManager, ITokenService tokenSe
       return ServiceResult<UserDto>.Failure(401, "Invalid username or password");
     }
 
-    // return ServiceResult<UserDto>.Failure(401, "Invalid username or password");
-
     var userDto = new UserDto
     {
       Username = user.UserName,
