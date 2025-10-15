4ab0a4128a928798550572a6cf1f2951648b61fc
robchendev <robchendev@gmail.com>
2025-10-14 19:45:38 -0700

    [fix] allow hyphens in username on FE

--- Frontend/src/app/components/register/register.component.ts
+++ Frontend/src/app/components/register/register.component.ts
@@ -41,7 +41,7 @@ export class RegisterComponent implements OnInit {
           Validators.required,
           Validators.minLength(3),
           Validators.maxLength(20),
-          Validators.pattern(/^[a-zA-Z0-9]+$/),
+          Validators.pattern(/^[a-zA-Z0-9-]+$/),
         ],
       ],
       password: [
