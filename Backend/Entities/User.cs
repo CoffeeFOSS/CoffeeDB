82791299f1133de7f91c7339f83f17625d7cee05
robchendev <robchendev@gmail.com>
2025-10-16 02:32:25 -0700

    [feat] LastUsernameChangedAt property

--- Backend/Entities/User.cs
+++ Backend/Entities/User.cs
@@ -5,6 +5,7 @@ namespace Backend.Entities;
 public class User : IdentityUser<int>
 {
   public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-  public DateTime LastActive { get; set; } = DateTime.UtcNow;
+  public DateTime LastActive { get; set; } = DateTime.UtcNow; // TODO: Make sure this is used, or remove it
+  public DateTime? LastUsernameChangedAt { get; set; }
   public ICollection<UserRole> UserRoles { get; set; } = [];
 }
