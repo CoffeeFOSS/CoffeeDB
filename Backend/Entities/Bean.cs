fb10a2a6ae469529fb0073bc07b99dcbf7611dbc
robchendev <robchendev@gmail.com>
2025-10-24 18:07:02 -0700

    [data] created bean entity

---
+++ Backend/Entities/Bean.cs
@@ -0,0 +1,20 @@
+namespace Backend.Entities;
+
+public class Bean : BaseEntity
+{
+  public string Name { get; set; } = string.Empty;
+  public bool Decaf { get; set; }
+  public int? Elevation { get; set; } // in masl (metres above sea level)
+  public string? Roast { get; set; }
+  public string? Type { get; set; }
+  public string? Region { get; set; }
+  public string? Farm { get; set; }
+  public string? Varietal { get; set; }
+  public string? Producer { get; set; }
+  public string? Importer { get; set; }
+  public string? Process { get; set; }
+  public string? FlavorProfile { get; set; }
+  public DateTime ReleaseDate { get; set; }
+
+  // Constraint: (Roaster, Name) must be unique
+}
