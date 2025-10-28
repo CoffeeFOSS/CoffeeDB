c445ee7be23899ac082ec328bb1879343ba109b9
robchendev <robchendev@gmail.com>
2025-10-27 22:55:13 -0700

    [data] enforce data constraints for GrindingElements

--- Backend/Data/Configurations/GrindingElementConfiguration.cs
+++ Backend/Data/Configurations/GrindingElementConfiguration.cs
@@ -8,6 +8,11 @@ public class GrindingElementConfiguration : IEntityTypeConfiguration<GrindingEle
 {
   public void Configure(EntityTypeBuilder<GrindingElement> builder)
   {
+    builder.ToTable(tb => tb.HasCheckConstraint(
+      "CK_GrindingElement_Diameter_Positive",
+      $"{nameof(GrindingElement.Diameter)} >= 0"
+    ));
+
     // GrindingElement > GrindingMechanism
     builder
       .HasOne(ge => ge.GrindingMechanism)
