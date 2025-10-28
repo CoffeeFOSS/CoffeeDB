3af64b15f1c8a825a7650b3aff8e7755af4e7645
robchendev <robchendev@gmail.com>
2025-10-27 22:52:50 -0700

    [data] enforce data constraints for GrinderDials

--- Backend/Data/Configurations/GrinderDialConfiguration.cs
+++ Backend/Data/Configurations/GrinderDialConfiguration.cs
@@ -8,6 +8,9 @@ public class GrinderDialConfiguration : IEntityTypeConfiguration<GrinderDial>
 {
   public void Configure(EntityTypeBuilder<GrinderDial> builder)
   {
+    builder.Property(gd => gd.Name).HasMaxLength(50);
+    builder.Property(gd => gd.Comment).HasMaxLength(500);
+
     // GrinderDial > Grinder
     builder
       .HasOne(gd => gd.Grinder)
