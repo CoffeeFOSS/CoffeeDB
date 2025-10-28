9ccecd201043f52a37acbee7635ec6aac7209d1a
robchendev <robchendev@gmail.com>
2025-10-27 22:47:30 -0700

    [data] enforce data constraints for Grinders

--- Backend/Data/Configurations/GrinderConfiguration.cs
+++ Backend/Data/Configurations/GrinderConfiguration.cs
@@ -8,6 +8,15 @@ public class GrinderConfiguration : IEntityTypeConfiguration<Grinder>
 {
   public void Configure(EntityTypeBuilder<Grinder> builder)
   {
+    builder.Property(g => g.Model).HasMaxLength(100);
+    builder.Property(g => g.ModelAlias).HasMaxLength(200);
+    builder.Property(g => g.Description).HasMaxLength(2000);
+
+    builder.ToTable(tb => tb.HasCheckConstraint(
+      "CK_Grinder_ReleaseDate_8Digits",
+      $"({nameof(Grinder.ReleaseDate)} IS NULL) OR ({nameof(Grinder.ReleaseDate)} >= 10000000 AND {nameof(Grinder.ReleaseDate)} <= 99999999)"
+    ));
+
     // Grinder > Brand
     builder
       .HasOne(g => g.Brand)
