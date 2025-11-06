60de1c7986f1d6c6f417876dded4e37bd942a71b
robchendev <robchendev@gmail.com>
2025-11-05 22:04:52 -0800

    [refactor] use abstract class for roaster directory

--- Frontend/src/app/components/roaster-directory/roaster-directory.component.ts
+++ Frontend/src/app/components/roaster-directory/roaster-directory.component.ts
@@ -1,41 +1,21 @@
-import {
-  Component,
-  effect,
-  inject,
-  OnInit,
-  signal,
-  untracked,
-} from '@angular/core';
-import {
-  buildParamsFromForm,
-  buildParamsValueDefaults,
-  createPaginationSignals,
-  fetchItemsWithCache,
-  sanitizeObjectFields,
-  subscribeToQueryParams,
-  syncParamsWithUrl,
-} from '../../utils/params.utils';
+import { Component, inject, signal } from '@angular/core';
+import { sanitizeObjectFields } from '../../utils/params.utils';
 import { RoastersService } from '../../services/roasters.service';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { AuthDirective } from '../../directive/auth.directive';
 import { PaginationControlsComponent } from '../pagination-controls/pagination-controls.component';
-import { LoadingService } from '../../services/loading.service';
 import { Roaster } from '../../models/roaster';
-import { PaginatedResult } from '../../models/pagination';
-import { getPaginationText } from '../../utils/pagination.utils';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 import { TextInputComponent } from '../forms/text-input/text-input.component';
 import {
   allControlsGroupFilled,
   requireAllControlsValidator,
-  setSubmittedAndValidateForm,
 } from '../../utils/form.utils';
 import { ErrorTextComponent } from '../error-text/error-text.component';
+import {
+  FormKeyMap,
+  PaginatedDirectoryComponent,
+} from '../abstract/paginated-directory/paginated-directory.component';
 
 @Component({
   selector: 'app-roasters',
@@ -50,117 +30,59 @@ import { ErrorTextComponent } from '../error-text/error-text.component';
   templateUrl: './roaster-directory.component.html',
   styleUrl: './roaster-directory.component.scss',
 })
-export class RoasterDirectoryComponent implements OnInit {
-  protected roastersService = inject(RoastersService);
-  router = inject(Router);
-  private route = inject(ActivatedRoute);
-  private cache: Record<string, PaginatedResult<Roaster[]>> = {};
-  private fb = new FormBuilder();
-  loadingService = inject(LoadingService);
-  searchForm: FormGroup = new FormGroup({});
-  validationErrors: string[] = [];
-  paginationSignals = createPaginationSignals();
-  roasters = signal<Roaster[]>([]);
-  paginatedResultSignal = signal<PaginatedResult<Roaster[]> | null>(null);
-  submitted = signal(false);
-  loadingKey = 'roaster-directory';
-
-  private coordinateControlNames = ['lat', 'long', 'radius'];
-  private formKeyMap: Record<string, { paramCode: string; default: any }> = {
-    name: { paramCode: 'n', default: '' },
-    address: { paramCode: 'a', default: '' },
-    lat: { paramCode: 'la', default: '' },
-    long: { paramCode: 'lo', default: '' },
-    radius: { paramCode: 'r', default: '' },
+export class RoasterDirectoryComponent extends PaginatedDirectoryComponent<
+  Roaster,
+  RoastersService
+> {
+  protected service = inject(RoastersService);
+  protected items = signal<Roaster[]>([]);
+  protected loadingKey = 'roaster-directory';
+  protected formKeyMap: FormKeyMap = {
+    name: {
+      paramCode: 'n',
+      default: '',
+      validators: [Validators.maxLength(100)],
+    },
+    address: {
+      paramCode: 'a',
+      default: '',
+      validators: [Validators.maxLength(200)],
+    },
+    lat: {
+      paramCode: 'la',
+      default: '',
+      validators: [Validators.min(-90), Validators.max(90)],
+    },
+    long: {
+      paramCode: 'lo',
+      default: '',
+      validators: [Validators.min(-180), Validators.max(180)],
+    },
+    radius: {
+      paramCode: 'r',
+      default: '',
+      validators: [Validators.min(0.1), Validators.max(15000)],
+    },
   };
 
-  constructor() {
-    effect(() => {
-      this.paginationSignals.page.signal();
-      this.paginationSignals.pageSize.signal();
-      untracked(() => {
-        this.syncParamsWithUrl();
-        this.fetchItems();
-      });
+  private coordinateControlNames = ['lat', 'long', 'radius'];
+
+  protected override getFormGroupValidators() {
+    return {
+      validators: requireAllControlsValidator(this.coordinateControlNames),
+    };
+  }
+
+  protected fetchPaginatedItems() {
+    return this.service.getRoasters({
+      page: this.paginationSignals.page.signal(),
+      pageSize: this.paginationSignals.pageSize.signal(),
+      ...sanitizeObjectFields(this.searchForm.value),
     });
-  }
-
-  ngOnInit(): void {
-    this.initializeForm();
-    subscribeToQueryParams({
-      route: this.route,
-      formGroup: this.searchForm,
-      paginationSignals: this.paginationSignals,
-      formKeyMap: this.formKeyMap,
-      submittedSignal: this.submitted,
-      validationErrors: this.validationErrors,
-    });
-  }
-
-  initializeForm() {
-    const { name, address, lat, long, radius } = this.formKeyMap;
-    this.searchForm = this.fb.group(
-      {
-        name: [name.default, [Validators.maxLength(100)]],
-        address: [address.default, [Validators.maxLength(200)]],
-        lat: [lat.default, [Validators.min(-90), Validators.max(90)]],
-        long: [long.default, [Validators.min(-180), Validators.max(180)]],
-        radius: [radius.default, [Validators.min(0.1), Validators.max(15000)]],
-      },
-      {
-        validators: requireAllControlsValidator(this.coordinateControlNames),
-      },
-    );
-  }
-
-  fetchItems() {
-    fetchItemsWithCache({
-      cache: this.cache,
-      itemsSignal: this.roasters,
-      paginationSignals: this.paginationSignals,
-      params: buildParamsFromForm(this.searchForm.value, this.formKeyMap),
-      loadingKey: this.loadingKey,
-      loadingService: this.loadingService,
-      resultSignal: this.paginatedResultSignal,
-      fetchPaginatedItems: () =>
-        this.roastersService.getRoasters({
-          page: this.paginationSignals.page.signal(),
-          pageSize: this.paginationSignals.pageSize.signal(),
-          ...sanitizeObjectFields(this.searchForm.value),
-        }),
-    });
-  }
-
-  syncParamsWithUrl() {
-    syncParamsWithUrl({
-      router: this.router,
-      route: this.route,
-      paginationSignals: this.paginationSignals,
-      params: buildParamsValueDefaults(this.searchForm.value, this.formKeyMap),
-    });
-  }
-
-  onSearchSubmit() {
-    setSubmittedAndValidateForm(
-      this.submitted,
-      this.searchForm,
-      this.validationErrors,
-    );
-    this.syncParamsWithUrl();
-    this.fetchItems();
-  }
-
-  onResetSearch() {
-    this.searchForm.reset();
-    this.onSearchSubmit();
-  }
-
-  get paginationText(): string {
-    return getPaginationText(this.paginatedResultSignal());
   }
 
   get isDistanceProvided(): boolean {
-    return this.roasters().some(
+    return this.items().some(
       (roaster) => roaster.distanceInKilometers !== null,
     );
   }
