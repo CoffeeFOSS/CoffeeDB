51439e5fe01da365db0e72751a6da27e69f92189
robchendev <robchendev@gmail.com>
2025-11-11 22:37:41 -0800

    [test] service tests for user service

---
+++ Backend.Tests/UserServiceTests.cs
@@ -0,0 +1,69 @@
+using Backend.Common;
+using Backend.Common.Params;
+using Backend.DTOs;
+using Backend.Interfaces.Repository;
+using Backend.Interfaces.Services;
+using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace Backend.Tests;
+
+public class UserServiceTests
+{
+  private readonly Mock<IUserRepository> _mockRepo = new();
+  private readonly DefaultHttpContext _httpContext = new();
+  private readonly IUserService _userService;
+
+  public UserServiceTests()
+  {
+    _userService = new UserService(_mockRepo.Object);
+  }
+
+  [Fact]
+  public async Task GetUserAsync_UserExists_ReturnsSuccess()
+  {
+    var username = "test";
+    var memberDto = new MemberDto { Id = 0, Username = username };
+    _mockRepo.Setup(r => r.GetMemberAsync(username)).ReturnsAsync(memberDto);
+
+    var result = await _userService.GetUserAsync(username);
+
+    Assert.True(result.IsSuccess);
+    Assert.Equal(200, result.StatusCode);
+    Assert.Equal(username, result.Data?.Username);
+  }
+
+  [Fact]
+  public async Task GetUserAsync_UserDoesNotExist_ReturnsFailure()
+  {
+    var username = "test";
+    _mockRepo.Setup(r => r.GetMemberAsync(username)).ReturnsAsync((MemberDto?)null);
+
+    var result = await _userService.GetUserAsync(username);
+
+    Assert.False(result.IsSuccess);
+    Assert.Equal(404, result.StatusCode);
+    Assert.Null(result.Data?.Username);
+  }
+
+  [Fact]
+  public async Task GetUsersAsync_CalledWithParams_AddsPaginationHeaderAndReturnsPagedList()
+  {
+    var userParams = new UserParams { Username = "bar" };
+    var users = new List<MemberDto>
+    {
+      new() { Id = 1, Username = "bart" },
+      new() { Id = 1, Username = "abarn" },
+    };
+    var pagedList = new PagedList<MemberDto>(users, users.Count, page: 1, pageSize: 10);
+    _mockRepo.Setup(r => r.GetMembersAsync(It.IsAny<UserParams>())).ReturnsAsync(pagedList);
+
+    var result = await _userService.GetUsersAsync(userParams, _httpContext.Response);
+
+    Assert.Equal(2, result.Count);
+    Assert.Contains(result, u => u.Username == "bart");
+    Assert.Contains(result, u => u.Username == "abarn");
+    Assert.True(_httpContext.Response.Headers.ContainsKey("Pagination"));
+  }
+}
